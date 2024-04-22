701718aeab05d3e95d26651acf8b90fcebe6ee4f
itz3406 <119579322+itz3406@users.noreply.github.com>
2024-04-22 15:13:17 -0400

    Update profile.dart

--- ecommerce_app/lib/pages/profile.dart
+++ ecommerce_app/lib/pages/profile.dart
@@ -1,15 +1,67 @@
+//user can add username and save it
 import 'package:flutter/material.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({super.key});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  State<Profile> createState() => _CartState();
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _CartState extends State<Profile> {
+class _ProfileScreenState extends State<ProfileScreen> {
+  TextEditingController _usernameController = TextEditingController();
+  String _username = "";
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text("Profile"),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Username:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Enter your username",
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _username = _usernameController.text;
+                  _usernameController.text = _username; // 
+                });
+              
+              },
+              child: Text("Save"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Saved Username: $_username",
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
